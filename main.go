a5288e4b91a3532826139d2d491f71ad0dd2bb70
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-07-22 09:51:36 +0900

    using static/index.html

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import "github.com/kataras/iris"
 
 func main() {
 	app := iris.Default()
+
+	tmpl := iris.HTML("./static", ".html")
+	app.RegisterView(tmpl)
+
 	app.Get("/hello", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{
 			"message": "Hello Iris",
 		})
+	})
+
+	app.Get("/", func(ctx iris.Context) {
+		ctx.View("index.html")
 	})
 
 	app.Run(iris.Addr(":3000"))
